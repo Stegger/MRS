161a3ba925e143eeda124eb717a0758a8c08cd71
Peter Gaarsmand Stegger Nielsen <pgn@easv.dk>
2018-11-12 14:06:24 +0100

    Initial commit

---
+++ src/movierecsys/dal/FileReaderTester.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movierecsys.dal;
+
+import java.io.IOException;
+import java.util.List;
+import movierecsys.be.Movie;
+
+/**
+ *
+ * @author pgn
+ */
+public class FileReaderTester
+{
+
+    /**
+     * Example method. This is the code I used to create the users.txt files.
+     *
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException
+    {
+        MovieDAO movieDao = new MovieDAO();
+        Movie movie = movieDao.createMovie(1966, "Django");
+        System.out.println(movie);
+    }
+}
---
+++ src/movierecsys/dal/MovieDAO.java
@@ -0,0 +1,164 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movierecsys.dal;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.MalformedInputException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+import movierecsys.be.Movie;
+
+/**
+ *
+ * @author pgn
+ */
+public class MovieDAO
+{
+
+    private static final String MOVIE_SOURCE = "data/movie_titles.txt";
+
+    /**
+     * Gets a list of all movies in the persistence storage.
+     *
+     * @return List of movies.
+     */
+    public List<Movie> getAllMovies() throws IOException
+    {
+        List<Movie> allMovies = new ArrayList<>();
+        String source = "data/movie_titles.txt";
+        File file = new File(source);
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file)))
+        {
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                try
+                {
+                    Movie mov = stringArrayToMovie(line);
+                    allMovies.add(mov);
+                } catch (Exception ex)
+                {
+                    //Do nothing
+                }
+            }
+        }
+        return allMovies;
+    }
+
+    /**
+     * Reads a movie from a , s
+     *
+     * @param t
+     * @return
+     * @throws NumberFormatException
+     */
+    private Movie stringArrayToMovie(String t)
+    {
+        String[] arrMovie = t.split(",");
+
+        int id = Integer.parseInt(arrMovie[0]);
+        int year = Integer.parseInt(arrMovie[1]);
+        String title = arrMovie[2];
+
+        Movie mov = new Movie(id, year, title);
+        return mov;
+    }
+
+    /**
+     * Creates a movie in the persistence storage.
+     *
+     * @param releaseYear The release year of the movie
+     * @param title The title of the movie
+     * @return The object representation of the movie added to the persistence
+     * storage.
+     */
+    public Movie createMovie(int releaseYear, String title) throws IOException
+    {
+        Path path = new File(MOVIE_SOURCE).toPath();
+        int id = -1;
+        try (BufferedWriter bw = Files.newBufferedWriter(path, StandardOpenOption.SYNC, StandardOpenOption.APPEND, StandardOpenOption.WRITE))
+        {
+            id = getNextAvailableMovieID();
+            bw.newLine();
+            bw.write(id + "," + releaseYear + "," + title);
+        }
+        return new Movie(id, releaseYear, title);
+    }
+
+    private int getNextAvailableMovieID() throws IOException
+    {
+        Path path = new File(MOVIE_SOURCE).toPath();
+        Stream<String> stream = Files.lines(path);
+        String highIdLine = stream.max(new Comparator<String>()
+        {
+            @Override
+            public int compare(String o1, String o2)
+            {
+                int id1, id2;
+                try
+                {
+                    id1 = Integer.parseInt(o1.split(",")[0]);
+                } catch (NumberFormatException nfe)
+                {
+                    id1 = -1;
+                }
+                try
+                {
+                    id2 = Integer.parseInt(o2.split(",")[0]);;
+                } catch (NumberFormatException mfe) 
+                {
+                    id2 = -1;
+                }
+                return Integer.compare(id1, id2);
+            }
+        }).get();
+        return Integer.parseInt(highIdLine.split(",")[0]) + 1;
+    }
+
+    /**
+     * Deletes a movie from the persistence storage.
+     *
+     * @param movie The movie to delete.
+     */
+    private void deleteMovie(Movie movie)
+    {
+        //TODO Delete movie
+    }
+
+    /**
+     * Updates the movie in the persistence storage to reflect the values in the
+     * given Movie object.
+     *
+     * @param movie The updated movie.
+     */
+    private void updateMovie(Movie movie)
+    {
+        //TODO Update movies
+    }
+
+    /**
+     * Gets a the movie with the given ID.
+     *
+     * @param id ID of the movie.
+     * @return A Movie object.
+     */
+    private Movie getMovie(int id)
+    {
+        //TODO Get one Movie
+        return null;
+    }
+
+}
