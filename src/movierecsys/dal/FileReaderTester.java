ded7ea72d4d434629ee157f8e01e5be255b34a0a
Peter Gaarsmand Stegger Nielsen <pgn@easv.dk>
2018-11-19 20:31:51 +0100

    Added some UNTESTED batch inserts for users and ratings.

--- src/movierecsys/dal/FileReaderTester.java
+++ src/movierecsys/dal/FileReaderTester.java
@@ -11,13 +11,12 @@ import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 import movierecsys.be.Movie;
 import movierecsys.be.Rating;
+import movierecsys.be.User;
 
 /**
  *
@@ -34,8 +33,90 @@ public class FileReaderTester
      */
     public static void main(String[] args) throws IOException
     {
-        mitigateMovies();
+        //mitigateMovies();
+        mitigateUsers();
+        mitigateRatings();
+    }
 
+    public static void mitigateUsers()
+    {
+        SQLServerDataSource ds = new SQLServerDataSource();
+        ds.setServerName("10.176.111.31");
+        ds.setDatabaseName("mrs");
+        ds.setUser("CS2018A_40");
+        ds.setPassword("CS2018A_40");
+
+        List<User> users = new UserDAO().getAllUsers();
+
+        try (Connection con = ds.getConnection())
+        {
+            Statement statement = con.createStatement();
+            int counter = 0;
+            for (User user : users)
+            {
+                String sql = "INSERT INTO User (id,name) VALUES("
+                        + user.getId() + ",'"
+                        + user.getName() + "');";
+                statement.addBatch(sql);
+                counter++;
+                if (counter % 1000 == 0)
+                {
+                    statement.executeBatch();
+                    System.out.println("Added 1000 users.");
+                }
+            }
+            if (counter % 1000 != 0)
+            {
+                statement.executeBatch();
+                System.out.println("Added final batch of users.");
+            }
+        } catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Untested batch insert. We'll see how it goes tomorrow.
+     *
+     * @throws IOException
+     */
+    public static void mitigateRatings() throws IOException
+    {
+        List<Rating> allRatings = new RatingDAO().getAllRatings();
+        SQLServerDataSource ds = new SQLServerDataSource();
+        ds.setServerName("10.176.111.31");
+        ds.setDatabaseName("mrs");
+        ds.setUser("CS2018A_40");
+        ds.setPassword("CS2018A_40");
+        try (Connection con = ds.getConnection())
+        {
+            Statement st = con.createStatement();
+            int counter = 0;
+            for (Rating rating : allRatings)
+            {
+                String sql = "INSERT INTO Rating (movieId, userId, score) VALUES ("
+                        + rating.getMovie() + ","
+                        + rating.getUser() + ","
+                        + rating.getRating()
+                        + ");";
+                st.addBatch(sql);
+                counter++;
+                if (counter % 1000 == 0)
+                {
+                    st.executeBatch();
+                    System.out.println("Added 1000 ratings.");
+                }
+            }
+            if (counter % 1000 != 0)
+            {
+                st.executeBatch();
+                System.out.println("Added final batch of ratings.");
+            }
+        } catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
     }
 
     public static void mitigateMovies() throws IOException
@@ -52,7 +133,7 @@ public class FileReaderTester
         try (Connection con = ds.getConnection())
         {
             Statement statement = con.createStatement();
-            
+
             for (Movie movie : movies)
             {
                 String sql = "INSERT INTO Movie (id,year,title) VALUES("
